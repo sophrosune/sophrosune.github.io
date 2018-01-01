21e5fe1e2dfe661decf123d9dac2397738042dc2
sophrosune <hadasbt@gmail.com>
2018-01-01 14:30:11 -0500

    jquery started working!

--- HBTHR/hbthrgame1.js
+++ HBTHR/hbthrgame1.js
@@ -1,31 +1,9 @@
-
-var correctCards = 0;
-$( init );
- 
-function init() {
- 
-  // Hide the success message
-  $('#successMessage').hide();
-  $('#successMessage').css( {
-    left: '580px',
-    top: '250px',
-    width: 0,
-    height: 0
-  } );
- 
-  // Reset the game
-  correctCards = 0;
-  $('#recardPile').html( '' );
-  $('#recardSlots').html( '' );
-
-
-function init() {
-  $('#hb1').draggable({
-  	containment: '#content',
-    cursor: 'move',
-    snap: '#content'
-stop: handleDragStop});
-  $('#hb1').droppable( {
-    drop: handleDropEvent
-  } );
-}
+$( function() {
+$(".card-img-top").draggable();
+$(".textcard").droppable({
+  drop: function(event, ui){
+ var draggable = ui.draggable;
+ console.log(draggable.attr('id'));
+  }
+});
+});
