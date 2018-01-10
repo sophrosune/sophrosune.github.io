4fd03e70c7a839c07734a33617edc46596429fe0
sophrosune <hadasbt@gmail.com>
2018-01-10 00:24:21 -0500

    added old versions

--- HBTHR/hbthrgame1.js
+++ HBTHR/hbthrgame1.js
@@ -3,7 +3,37 @@ $(".card-img-top").draggable();
 $(".textcard").droppable({
   drop: function(event, ui){
  var draggable = ui.draggable;
- console.log(draggable.attr('id'));
+ // console.log(draggable.attr('id'));
   }
+})
 });
+
+// $(function() {
+//   $('.recardPile').draggable(
+//     {revert: 'invalid',
+//     stop: function(){
+//       $(this).draggable('option','revert','invalid')
+//     }
+//   }
+//   );
+
+//   dropped('.hb1', 'hb1');
+
+
+// });
+
+
+$( function() {
+$(".card-img-top").draggable();
+$(".textcard").droppable({
+  drop: function(event, ui){
+ var draggable = ui.draggable;
+ if(draggable.attr('id')===recardPile) {
+  alert('that is right!');
+ //  $(this).addClass('correct').fadeIn('slow').css('background-color', '#D14428' ).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+ //    $(this).removeClass('animated shake');
+ // })
+ }
+  } 
+})
 });
---
+++ HBTHR/hbthrwholanding1.js
@@ -0,0 +1,43 @@
+// $('.HBT').click(changeImg);
+// function changeImg() {
+// const url = $(this).attr('src')
+// $('.box1').attr('src', url)
+// }
+
+// $('.box1').hide('fast')
+
+// const showIt = () => {
+//  // $('#div1').show(2000)
+//  $('.HBT').hide('fast');
+//   $('.box1').show('fast')
+// }
+
+// const showIt2 = () => {
+//  // $('#div1').show(2000)
+//  $('.box1').hide('fast');
+//   $('.HBT').show('fast')
+// }
+
+// $('.box1').click(showIt2)
+// $('.HBT').click(showIt)
+
+
+// $('#teams').change(updateHTML)
+
+// function updateHTML() {
+//   const team = $(this).val(); 
+//   const body = $('.teams div').removeClass().addClass('team')
+  
+// }
+
+
+
+    $(document).ready(function() {
+        $('nav').hide('fast');
+        $('#teams').change(function() {
+            $('.section').hide();
+            $('#' + $(this).val()).show();
+        });
+    });
+
+
